548177604f4c9b988c9d27a584af0a354404f261
Maciej <msajdera@gmail.com>
2020-05-04 03:09:56 +0200

    cleared cache and public container added

--- src/components/order-button.js
+++ src/components/order-button.js
@@ -9,13 +9,13 @@ const OrderButton = () => {
       <Helmet>
       <script src="https://store.goorder.pl/goorder.js"></script>
       </Helmet>
- 
+      <div class="goorder" data-src="https://bullpub.goorder.pl/widget">
       <button data-goorder-open class="goorder" data-goorder-modal="1" data-src="https://bullpub.goorder.pl">
 
       <img id={`widget-logo`} src="https://www.datocms-assets.com/26240/1588543997-goorder-icon-dark.png"></img>
 
       </button>
-
+      </div>
       </>
     );
   
