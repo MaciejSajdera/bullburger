680221f6e31654a1ff6e8ec132cf0bbf98206f6f
Maciej <msajdera@gmail.com>
2020-05-04 02:41:08 +0200

    div container from goorder added -test

--- src/components/order-button.js
+++ src/components/order-button.js
@@ -9,6 +9,7 @@ const OrderButton = () => {
       <Helmet>
       <script src="https://store.goorder.pl/goorder.js"></script>
       </Helmet>
+      <div class="goorder" data-src="https://bullpub.goorder.pl/widget">
       <button id={`goorderButton`} className={`goorder`} data-goorder-open class="goorder" data-goorder-modal="1" data-src="https://bullpub.goorder.pl">
 
       <img id={`widget-logo`} src="https://www.datocms-assets.com/26240/1588543997-goorder-icon-dark.png"></img>
@@ -17,6 +18,7 @@ const OrderButton = () => {
         {props.navScrolled ? <img id={`widget-logo`} src="https://www.datocms-assets.com/26240/1588543997-goorder-icon-dark.png"></img> : <img id={`widget-logo`} src="https://www.datocms-assets.com/26240/1588543443-goorder-icon.png"></img>} */}
         
         </button>
+        </div>
       </>
     );
   
