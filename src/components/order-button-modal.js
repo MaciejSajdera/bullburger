dcf2088d55e40720522e3c1b56c7d8d08d0a74e7
hoodini47 <msajdera@gmail.com>
2020-11-23 18:25:20 +0100

    goorder changed to ordersmart.

--- src/components/order-button-modal.js
+++ src/components/order-button-modal.js
@@ -1,27 +1,30 @@
-import React from 'react';
-import {Helmet} from "react-helmet"
+import React from "react"
+// import {Helmet} from "react-helmet"
 import "../styles/goorder.scss"
- 
+
 export const OrderButtonModal = () => {
-
-    return (
-      <div id={`widget-modal-wrapper`}>
-
-      <Helmet>
+  return (
+    <div id={`widget-modal-wrapper`}>
+      {/* <Helmet>
       <script src="https://store.goorder.pl/goorder.js"></script>
-      </Helmet>
-      
-      <button id={`goorderButton`} className={`goorder`} data-goorder-open data-goorder-modal="1" data-src="https://bullpub.goorder.pl">
+      </Helmet> */}
 
-        <img id={`widget-logo`} src="https://www.datocms-assets.com/26240/1588543997-goorder-icon-dark.png"></img>
+      <a
+        id={`goorderButton`}
+        className={`goorder`}
+        href="https://bullburger.ordersmart.pl/2354"
+        target="_blank"
+      >
+        <img
+          id={`widget-logo`}
+          src="https://www.datocms-assets.com/26240/1606151740-order-smart-logo-retina-2x-1.png"
+        ></img>
 
         {/*        //warunkowo zmiana z bialego na czarne
         {props.navScrolled ? <img id={`widget-logo`} src="https://www.datocms-assets.com/26240/1588543997-goorder-icon-dark.png"></img> : <img id={`widget-logo`} src="https://www.datocms-assets.com/      26240/1588543443-goorder-icon.png"></img>} */}
-
-        </button>
-
-      </div>
-    );
+      </a>
+    </div>
+  )
 }
 
 export default OrderButtonModal
